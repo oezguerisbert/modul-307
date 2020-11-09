5e723c1ce7156334c22d63bfcdb33c0cde6f7e38
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-09 11:08:56 +0100

    new file

---
+++ einbettung.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Titelzeile der Datei</title>
+</head>
+<body>
+Die erste Zeile in HTML<br />
+<?php echo "Die zweite Zeile in PHP<br />"; 
+// Kommentare schreibt man so 😊
+/*
+Das ist ebenfalls ein Kommentar
+*/
+?>
+Die dritte Zeile in HTML<br />
+<?php
+echo "Die vierte Zeile in PHP<br />";
+echo "Die fünfte Zeile in PHP";
+?>
+</body>
+</html>
