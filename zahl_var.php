5a91517ef416f028f6a121ebe538a421685fc9a4
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-09 11:39:04 +0100

    updates

---
+++ zahl_var.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <?php
+        $liter = 14;
+        $preis = 1.35;
+        $zahlung = $liter * $preis;
+        echo "Resultat = $zahlung";
+        echo 'Resultat = $zahlung';
+    ?>
+</body>
+</html>
