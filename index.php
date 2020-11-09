de13a2cdc8cc327c1bda98af7f52827e429a2478
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-09 10:23:18 +0100

    some configs

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modul 307</title>
-</head>
-<body>
-    test
-</body>
-</html>
+<?php
+phpinfo();
