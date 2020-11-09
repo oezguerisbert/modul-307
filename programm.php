6cee6dabe4d9536db156d21cd5c301a69028df15
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-09 12:02:03 +0100

    updates

---
+++ programm.php
@@ -0,0 +1,14 @@
+<html>
+    <head></head>
+    <body>
+       <h1>Ich bin eine Ueberschrift</h1>
+       <p>Ich bin ein Absatz</p>
+        <?php
+        for ($i=0; $i < 100; $i++) { 
+            echo "Das ist die Zeile $i<br />";
+            echo "Test<br />";
+            
+        }
+        ?> 
+    </body>
+</html>
