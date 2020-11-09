9e9d44429dd6fbca2cc825487a323094e318a8f3
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-09 13:26:03 +0100

    updates

---
+++ willkommen.php
@@ -0,0 +1,18 @@
+<?php 
+$name = "Özgür Isbert";
+$modul = "307";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align:center">Herzlich willkommen zum ["Modul <?php echo $modul;?>"]</h1>
+    <h1 style="text-align:center">Herzlich willkommen <?php echo $name; ?> zum ["Modul <?php echo $modul;?>"]</h1>
+</body>
+</html>
