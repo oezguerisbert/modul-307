e20160d05b003b3d92e9a7e9b066725bbc07e428
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-09 14:15:28 +0100

    updates

---
+++ text.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+         echo "Eins: " . $_POST["eins"] . "<br />";
+         echo "Zwei: " . $_POST["zwei"] . "<br />";
+         echo "Drei: " . $_POST["drei"] . "<br />";
+         echo "Vier: " . $_POST["vier"] . "<br />";
+        ?>
+    </body>
+</html>
