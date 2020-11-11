0f4816e0c2d343dfe613f78a07d376a532589db3
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-11 13:31:09 +0100

    tag 2

---
+++ mittel.inc.php
@@ -0,0 +1,6 @@
+<?php
+function mittel($n1, $n2, $n3) {
+    
+    echo "Der Mittelwert von $n1, $n2 und $n3 ist ".(($n1+$n2+$n3)/3)."<br />";
+}
+?>
---
+++ u_mehrere1.php
@@ -0,0 +1,7 @@
+<h1></h1>
+<?php
+include 'mittel.inc.php';
+mittel(1,2,3);
+mittel(2,3,4);
+mittel(3,4,5);
+?>
