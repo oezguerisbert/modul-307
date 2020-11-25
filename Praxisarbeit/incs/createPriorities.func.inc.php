d80731ebc989647b29e8e739339721a551aab7f3
Özgür Isbert <oezguerisbert@gmail.com>
2020-11-25 15:03:33 +0100

    fixes

--- Praxisarbeit/incs/createPriorities.func.inc.php
+++ Praxisarbeit/incs/createPriorities.func.inc.php
@@ -4,7 +4,7 @@ function createPriorities(array $options, string $selected = ""){
     foreach ($options as $key => $option) {
         $name = "prio".($key+1);
         $d .= "<div class=\"custom-control custom-radio\">
-            <input type=\"radio\" id=\"$name\" name=\"prio\" value=\"".strtolower($option)."\" class=\"custom-control-input\" required ".($option === $selected ? "checked" : "").">
+            <input type=\"radio\" id=\"$name\" name=\"prio\" value=\"".strtolower($option)."\" class=\"custom-control-input\" required ".(strtolower($option) === strtolower($selected) ? "checked" : "").">
             <label class=\"custom-control-label\" for=\"$name\">".$option."</label>
         </div>";
     }
